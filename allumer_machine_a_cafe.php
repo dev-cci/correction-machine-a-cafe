c9fed2aee81b924fc1fcfbb19231596009531e23
svadhis <imeian@gmail.com>
2022-02-16 12:04:49 +0100

    exemple interaction js

---
+++ allumer_machine_a_cafe.php
@@ -0,0 +1,47 @@
+<?php
+
+Class MachineACafe {
+    private $marque;
+    private $prix = 0.3;
+    private $cafe = 0;  // 0 = aucune dosette - 1 = dosette chargée - 2 = café prêt
+    public $enFonction = false;
+
+    public function __construct($marque) {
+        $this->marque = $marque;
+    }
+
+    public function powerSwitch() {
+        if(!$this->enFonction){
+            $this->enFonction = true;
+            echo("La machine " . $this->marque . " est en fonction." . "<br>");
+        }elseif($this->enFonction){
+            $this->enFonction = false;
+            echo("La machine ". $this->marque . " n'est plus en fonction." . "<br>");
+        }
+    }
+
+    public function mettreUneDosette() {
+        if($this->cafe === 1){
+            echo "Une dosette est déjà chargée". "<br>";
+        }else {
+            $this->cafe = 1;
+            echo "La dosette est chargée";
+        }
+    }
+
+    public function faireDuCafe() {
+if($this->enFonction === true AND $this->cafe === 1 ){
+   echo "le café coule à flot";
+     $this->cafe=2;
+} else {
+
+    echo "Pas de café";
+}
+    }
+}
+
+$machine = new MachineACafe("Senseo");
+
+$machine->powerSwitch();
+
+?>
--- index.php
+++ index.php
@@ -1,49 +1,17 @@
-<?php
+<div class="power">Power</div>
+<div class="etat-machine">Eteint</div>
 
-Class MachineACafe {
-    private $marque;
-    private $prix = 0.3;
-    private $cafe = 0;  // 0 = aucune dosette - 1 = dosette chargée - 2 = café prêt
-    private $enFonction = false;
+<script>
 
-    public function __construct($marque) {
-        $this->marque = $marque;
-    }
+let monBouton = document.querySelector('.power');
 
-    public function powerSwitch() {
-        if(!$this->enFonction){
-            $this->enFonction = true;
-            echo("La machine " . $this->marque . " est en fonction." . "<br>");
-        }elseif($this->enFonction){
-            $this->enFonction = false;
-            echo("La machine ". $this->marque . " n'est plus en fonction." . "<br>");
-        }
-    }
+monBouton.addEventListener('click', () => {
+    fetch('./allumer_machine_a_cafe.php')
+    .then(response => response.text())
+    .then(status => {
+        let etat = document.querySelector('.etat-machine');
+        etat.innerHTML = status;
+    })
+})
 
-    public function mettreUneDosette() {
-        if($this->cafe === 1){
-            echo "Une dosette est déjà chargée". "<br>";
-        }else {
-            $this->cafe = 1;
-            echo "La dosette est chargée";
-        }
-    }
-
-    public function faireDuCafe() {
-if($this->enFonction === true AND $this->cafe ===1 ){
-   echo "le café coule à flot";
-     $this->cafe=2;
-} else {
-
-    echo "Pas de café";
-}
-    }
-}
-
-$machine = new MachineACafe("Senseo");
-$machine->powerSwitch();
-$machine->mettreUneDosette();
-$machine->faireDuCafe();
-
-
-?>
+</script>
